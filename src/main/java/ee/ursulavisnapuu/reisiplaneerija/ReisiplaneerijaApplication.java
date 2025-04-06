2485e654c9d4c12d51e0b02565d2b23bc714a417
Ursula Visnapuu <visnapuuursula@gmail.com>
2025-04-06 15:33:35 +0300

    Lisatud Trip mudel koos põhiandmetega (sihtkoht, kuupäevad, arhiivistaatus)

---
+++ src/main/java/ee/ursulavisnapuu/reisiplaneerija/ReisiplaneerijaApplication.java
@@ -0,0 +1,13 @@
+package ee.ursulavisnapuu.reisiplaneerija;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReisiplaneerijaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReisiplaneerijaApplication.class, args);
+	}
+
+}
